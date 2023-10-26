742b000c202b390c41c987f62d99f53854d2385a
Cambuchi <49548018+Cambuchi@users.noreply.github.com>
2023-10-26 15:39:27 -0700

    create function to add items to list of items

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import './styles/main.css';
 // import modules
 import initialize from './scripts/initialize';
 
-import * as d3 from './scripts/d3.v3.min';
+import * as d3 from './scripts/d3';
 
-// IIFE to encapsulate site creation
-(() => {
-  // create the initial page structure
-  initialize();
-
-  var padding = { top: 20, right: 40, bottom: 0, left: 0 },
+const drawChart = (data) => {
+  document.getElementById('chart').innerHTML = '';
+  let padding = { top: 20, right: 40, bottom: 0, left: 0 },
     w = 650 - padding.left - padding.right,
     h = 650 - padding.top - padding.bottom,
     r = Math.min(w, h) / 2,
@@ -32,112 +29,35 @@ import * as d3 from './scripts/d3.v3.min';
         '#03C7DC',
         '#054BC5',
       ]);
-  var data = [
-    {
-      label: 'Cam',
-      value: 1,
-      question: 'Loves traveling and wheels with names.',
-    },
-    {
-      label: 'Damien',
-      value: 2,
-      question: 'Coming in hot and dual-wielding fire extinguishers.',
-    },
-    {
-      label: 'Boss Mike',
-      value: 3,
-      question:
-        "Suddenly you hear boss music playing, oh wait that's just Mike's computer straining under the load of 100 tabs.",
-    },
-    {
-      label: 'Brad',
-      value: 4,
-      question: "Today's standup is sponsored by... ",
-    },
-    {
-      label: 'Keith',
-      value: 5,
-      question:
-        'Keith-math is going to the store to get coffee but leaving with pinto beans because BEANS.',
-    },
-    {
-      label: 'PJ',
-      value: 6,
-      question: 'Platform wizard and also the remote working outfit of choice.',
-    },
-    {
-      label: '#1 Mike G.',
-      value: 7,
-      question: 'Undisputed, number one Mike.',
-    },
-    {
-      label: 'George',
-      value: 8,
-      question: 'King of the NE Jungle. Watch out for that JIRA!',
-    },
-    {
-      label: 'Ryan',
-      value: 9,
-      question: 'Unofficial SusEng father figure.',
-    },
-    {
-      label: 'Best Mike S.',
-      value: 10,
-      question: 'Unarguably THE best Mike.',
-    },
-    {
-      label: 'Ana',
-      value: 11,
-      question: 'Auggie is the unofficial SusEng mascot.',
-    },
-    {
-      label: 'Ben',
-      value: 12,
-      question: 'I heard he lives in a panel van.',
-    },
-    {
-      label: 'Jeffrey',
-      value: 13,
-      question:
-        'Strong contender to take it all home in the Best Meme-game NAVEX tournament.',
-    },
-    {
-      label: 'Simon',
-      value: 14,
-      question:
-        'Most likely following up on a ticket out of nowhere like John Cena.',
-    },
-    {
-      label: 'Kyle',
-      value: 15,
-      question: 'With big feet comes big responsibility.',
-    },
-  ];
-  var svg = d3
+
+  let svg = d3
     .select('#chart')
     .append('svg')
     .data([data])
     .attr('width', w + padding.left + padding.right)
     .attr('height', h + padding.top + padding.bottom);
-  var container = svg
+
+  let container = svg
     .append('g')
     .attr('class', 'chartholder')
     .attr(
       'transform',
       'translate(' + (w / 2 + padding.left) + ',' + (h / 2 + padding.top) + ')',
     );
-  var vis = container.append('g');
 
-  var pie = d3.layout
+  let vis = container.append('g');
+
+  let pie = d3.layout
     .pie()
     .sort(null)
     .value(function (d) {
       return 1;
     });
-  // declare an arc generator function
-  var arc = d3.svg.arc().outerRadius(r);
+
+  let arc = d3.svg.arc().outerRadius(r);
+
   // select paths, use arc generator to draw
-  var arcs = vis
+  let arcs = vis
     .selectAll('g.slice')
     .data(pie)
     .enter()
@@ -152,6 +72,7 @@ import * as d3 from './scripts/d3.v3.min';
     .attr('d', function (d) {
       return arc(d);
     });
+
   // add the text
   arcs
     .append('text')
@@ -169,9 +90,10 @@ import * as d3 from './scripts/d3.v3.min';
     })
     .attr('text-anchor', 'end')
     .text(function (d, i) {
-      return data[i].label;
+      return data[i];
     });
   container.on('click', spin);
+
   function spin(d) {
     container.on('click', null);
     //all slices have been seen, all done
@@ -181,7 +103,7 @@ import * as d3 from './scripts/d3.v3.min';
       container.on('click', null);
       return;
     }
-    var ps = 360 / data.length,
+    let ps = 360 / data.length,
       pieslice = Math.round(1440 / data.length),
       rng = Math.floor(Math.random() * 1440 + 360);
 
@@ -195,7 +117,15 @@ import * as d3 from './scripts/d3.v3.min';
     } else {
       oldpick.push(picked);
     }
+
     rotation += 90 - Math.round(ps / 2);
+
+    if (oldpick.length > 1) {
+      d3.select(
+        '.slice:nth-child(' + (oldpick[oldpick.length - 2] + 1) + ') text',
+      ).style('fill', '#222');
+    }
+
     vis
       .transition()
       .duration(1500)
@@ -204,12 +134,13 @@ import * as d3 from './scripts/d3.v3.min';
         //mark question as seen
         d3.select('.slice:nth-child(' + (picked + 1) + ') path').attr(
           'fill',
-          '#111',
+          '#222',
         );
         d3.select('.slice:nth-child(' + (picked + 1) + ') text').style(
           'fill',
           'white',
         );
+
         // replace blurb with current name
         let currentName = d3.select(
           '.slice:nth-child(' + (picked + 1) + ') text',
@@ -217,16 +148,17 @@ import * as d3 from './scripts/d3.v3.min';
         d3.selectAll('#blurb')[0][0]['textContent'] =
           "It's your turn " + currentName;
         //populate question
-        d3.select('#question').text(data[picked].question);
+        // d3.select('#question').text(data[picked].question);
         oldrotation = rotation;
 
         /* Get the result value from object "data" */
-        console.log(data[picked].value);
+        // console.log(data[picked].value);
 
         /* Comment the below line for restrict spin to sngle time */
         container.on('click', spin);
       });
   }
+
   //make arrow
   svg
     .append('g')
@@ -241,6 +173,7 @@ import * as d3 from './scripts/d3.v3.min';
     .append('path')
     .attr('d', 'M-' + r * 0.15 + ',0L0,' + r * 0.05 + 'L0,-' + r * 0.05 + 'Z')
     .style({ fill: 'white' });
+
   //draw spin circle
   container
     .append('circle')
@@ -248,6 +181,7 @@ import * as d3 from './scripts/d3.v3.min';
     .attr('cy', 0)
     .attr('r', 60)
     .style({ fill: 'white', cursor: 'pointer' });
+
   //spin text
   container
     .append('text')
@@ -258,15 +192,15 @@ import * as d3 from './scripts/d3.v3.min';
     .style({ 'font-weight': 'bold', 'font-size': '30px' });
 
   function rotTween(to) {
-    var i = d3.interpolate(oldrotation % 360, rotation);
+    let i = d3.interpolate(oldrotation % 360, rotation);
     return function (t) {
       return 'rotate(' + i(t) + ')';
     };
   }
 
   function getRandomNumbers() {
-    var array = new Uint16Array(1000);
-    var scale = d3.scale.linear().range([360, 1440]).domain([0, 100000]);
+    let array = new Uint16Array(1000);
+    let scale = d3.scale.linear().range([360, 1440]).domain([0, 100000]);
     if (
       window.hasOwnProperty('crypto') &&
       typeof window.crypto.getRandomValues === 'function'
@@ -275,10 +209,52 @@ import * as d3 from './scripts/d3.v3.min';
       console.log('works');
     } else {
       //no support for crypto, get crappy random numbers
-      for (var i = 0; i < 1000; i++) {
+      for (let i = 0; i < 1000; i++) {
         array[i] = Math.floor(Math.random() * 100000) + 1;
       }
     }
     return array;
   }
-})();
+};
+
+const initialData = [
+  'Cam',
+  'Damien',
+  'Magic Mike',
+  'Brad',
+  'Keith',
+  'PJ',
+  '#1 Mike G.',
+  'George',
+  'Ryan',
+  'Best Mike S.',
+  'Ana',
+  'Ben',
+  'Jeffrey',
+  'Simon',
+  'Kyle',
+];
+
+const addItem = (item, itemList) => {
+  if (itemList.includes(item)) {
+    console.log(`TODO: let user know ${item} already exists in list`);
+    return;
+  } else {
+    itemList.push(item);
+    populateItems(itemList);
+  }
+};
+
+const populateItems = (itemList) => {};
+
+const clearItemList = (itemList) => {};
+
+let itemList = [](
+  // IIFE to encapsulate site creation
+  () => {
+    // create the initial page structure
+    initialize();
+
+    drawChart(initialData);
+  },
+)();
--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,5 +1,7 @@
 // module for creating the content section
 
+import plusImage from '../assets/images/plus.png';
+
 const createContent = () => {
   // create content container
   const content = document.createElement('div');
@@ -13,13 +15,35 @@ const createContent = () => {
   const chart = document.createElement('div');
   chart.id = 'chart';
 
-  const question = document.createElement('div');
-  question.id = 'question';
+  // create the addItemsBar
+  const addItemsBar = document.createElement('div');
+  addItemsBar.id = 'add-items-bar';
+  // add the addItemsBar image
+  const addIcon = new Image();
+  addIcon.src = plusImage;
+  addIcon.id = 'plus-icon';
+  // add the addItemsBar text input
+  const addItems = document.createElement('input');
+  addItems.type = 'text';
+  addItems.id = 'add-items';
+  addItems.placeholder = 'Add items...';
+  // append search items into the addItemsBar
+  addItemsBar.append(addItems);
+  addItemsBar.append(addIcon);
+
+  // create the items list
+  const itemsContainer = document.createElement('div');
+  itemsContainer.id = 'itemsContainer';
+  const items = document.createElement('ul');
+  items.id = 'items';
+
+  itemsContainer.append(items);
 
   // // append all items into the content container
   content.append(blurb);
   content.append(chart);
-  content.append(question);
+  content.append(addItemsBar);
+  content.append(itemsContainer);
 
   return content;
 };
